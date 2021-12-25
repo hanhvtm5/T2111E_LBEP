33c055e7d6555484126a53b584dcb88f022aed3d
hanhvtm5 <dangnhth2111019@fpt.edu.vn>
2021-12-25 20:04:45 +0700

    Update Untitled2.cpp

--- PRACTICE1/Untitled2.cpp
+++ PRACTICE1/Untitled2.cpp
@@ -1,38 +1,32 @@
 #include <stdio.h>
 #include <conio.h>
 using namespace std;
-void nhapmang(int a[], int &n)
-
-{ printf("nhap so phan tu :" );
+void nhapmang(int a[], int &n){
+  printf("nhap so phan tu :" );
   scanf("%d",&n);
-  for(int i=0;i<n;i++)
-  { printf(" phan tu arr [%d] :",i);
+  for(int i=0; i<n; i++){ 
+    printf(" phan tu arr [%d] :",i);
     scanf("%d",&a[i]);
-  }
+    }
   
 }
-int xuatmang(int a[],int n)
- {
-  for(int i=0;i<n;i++)
-     {
-        printf("%d \t",a[i]);
-     }
- }
+int xuatmang(int a[],int n){
+    for(int i=0; i<n; i++){
+    	printf("%d \t",a[i]);
+    }
+}
  
-void daonguoc(int a[],int n)
-{
- for(int i=0;i<=n/2;i++)
-  {
+void daonguoc(int a[],int n){
+  for(int i=0; i<=n/2; i++){
    int temp=a[i];
    a[i]=a[n-1-i];
    a[n-1-i]=temp;
-  }
+   }
   
  printf("\nmang nguoc lai la: ");
- for(int i=0;i<n;i++)
-     {
-        printf("%d ",a[i]);
-     } 
+ for(int i=0; i<n; i++){
+    printf("%d",a[i]);
+    } 
 }
 
 int main()
